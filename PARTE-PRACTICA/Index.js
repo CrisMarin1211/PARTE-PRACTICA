da5c6a76a7d1b8136f92c9b36927e33316d8ce72
CrisMarin1211 <145811107+CrisMarin1211@users.noreply.github.com>
2024-02-09 17:53:37 -0500

    Add files via upload

---
+++ PARTE-PRACTICA/Index.js
@@ -0,0 +1,11 @@
+const studentGrade = [{ Nota: A }];
+
+const studentGrades = [100, 50, 65, 70, 85, 96, 45];
+const result = computeGrades(studentGrades);
+console.log(result);
+
+console = {
+	parsed: [A, F, D, C, B, A, F],
+	passed: 5,
+	lose: 2,
+};
